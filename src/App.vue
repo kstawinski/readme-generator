daa3de035661f02a88949f5f5e31b7412250fb3d
kstawinski <stawinskidev@gmail.com>
2020-02-02 22:09:07 +0100

    Code unification

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <style lang="scss">
-@import url('https://meyerweb.com/eric/tools/css/reset/reset.css');
-@import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap');
-body {
-  font-family: 'Roboto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  background-color: #27359c;
-  background-image: url("https://www.transparenttextures.com/patterns/arches.png");
-}
+  @import url('https://meyerweb.com/eric/tools/css/reset/reset.css');
+  @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap');
+  body {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    background-color: #27359c;
+    background-image: url("https://www.transparenttextures.com/patterns/arches.png");
+  }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,44 +24,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.footer {
-  position: absolute;
-  display: flex;
-  bottom: 30px;
-  width: 100%;
-  left: 0;
-  justify-content: center;
-  color: rgba(255, 255, 255, 0.6);
+  .footer {
+    position: absolute;
+    display: flex;
+    bottom: 30px;
+    width: 100%;
+    left: 0;
+    justify-content: center;
+    color: rgba(255, 255, 255, 0.6);
 
-  &-item {
-    &:not(:last-child):after {
-      content: '/';
-      margin: 0 10px;
-      opacity: 0.2;
+    &-item {
+      &:not(:last-child):after {
+        content: '/';
+        margin: 0 10px;
+        opacity: 0.2;
+      }
+    }
+
+    &-badge {
+      color: #ffffffd9;
+      font-size: 12px;
+      font-weight: 600;
+      text-transform: uppercase;
+      border: 1px solid #ffffff45;
+      padding: 3px 6px;
+      border-radius: 4px;
+    }
+
+    &-link {
+      color: rgba(255, 255, 255, 0.7);
+      text-decoration: none;
+      border-bottom: 1px solid #ffffff45;
+      font-weight: 500;
+      transition: .3s ease-in-out;
+
+      &:hover {
+        color: #fff;
+        border-color: #fff;
+      }
     }
   }
-
-  &-badge {
-    color: #ffffffd9;
-    font-size: 12px;
-    font-weight: 600;
-    text-transform: uppercase;
-    border: 1px solid #ffffff45;
-    padding: 3px 6px;
-    border-radius: 4px;
-  }
-
-  &-link {
-    color: rgba(255, 255, 255, 0.7);
-    text-decoration: none;
-    border-bottom: 1px solid #ffffff45;
-    font-weight: 500;
-    transition: .3s ease-in-out;
-
-    &:hover {
-      color: #fff;
-      border-color: #fff;
-    }
-  }
-}
 </style>
