c377dfc560736d03c6685f94c6b50be5ebecc6d5
kstawinski <stawinskidev@gmail.com>
2020-02-02 22:02:29 +0100

    Create main page and footer component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link v-bind:to="link" tag="button">{{ text }}</router-link>
+  <router-link v-bind:to="link" tag="button" class="button">{{ text }}</router-link>
 </template>
 
 <script>
@@ -14,4 +14,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.button {
+  font-family: 'Roboto', sans-serif;
+  cursor: pointer;
+  background: #fff;
+  font-size: 14px;
+  color: #26359c;
+  border: 1px solid #fff;
+  font-weight: 600;
+  padding: 10px 30px;
+  border-radius: 30px;
+  box-shadow: 0 2px 5px #00000026;
+  text-transform: uppercase;
+  transition: .3s cubic-bezier(0.65, 0.05, 0.36, 1);
+
+  &:hover {
+    background-color: #26359c;
+    border-color: rgba(255, 255, 255, 0.6);
+    color: #fff;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-    <h1>Homepage</h1>
-    <Button text="Go and create your README file" link="/create" />
+  <div class="wrapper">
+    <div class="main">
+      <h1 class="main-title">README Generator</h1>
+
+      <!-- eslint-disable max-len -->
+      <p class="main-text">A simple and usefull README file generator.</p>
+      <p class="main-text">Take care of your project. Just do it!</p>
+
+      <Button
+        text="Go and create your README file"
+        link="/create"
+        class="main-button"
+      />
+
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import Button from '@/components/Button.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'home',
-  components: { Button },
+  components: { Button, Footer },
 };
 </script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+  }
+  .main {
+    text-align: center;
+    color: #fff;
+
+    &-title {
+      font-weight: 300;
+      font-size: 35px;
+      margin-bottom: 35px;
+    }
+
+    &-text {
+      line-height: 1.35;
+      font-size: 24px;
+      font-weight: 300;
+      opacity: .6;
+    }
+
+    &-button { margin-top: 35px; }
+  }
+</style>
