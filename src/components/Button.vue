ac5ae16b7189efaa34a1be808bc79e2f90edd1ec
kstawinski <stawinskidev@gmail.com>
2020-02-02 20:33:36 +0100

    Create app wireframe

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-link v-bind:to="link" tag="button">{{ text }}</router-link>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    link: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/views/Create.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Create page</h1>
+</template>
