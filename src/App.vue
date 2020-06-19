9b08825efb3b62299627dddda8022e039d12b1ab
kstawinski <stawinskidev@gmail.com>
2020-06-19 12:01:06 +0200

    Remove unused styles

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,7 @@
 </template>
 
 <style lang="scss">
-@import url('https://meyerweb.com/eric/tools/css/reset/reset.css');
 @import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap');
-body {
-  font-family: 'Roboto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-
-  &.home {
-    background-color: #27359c;
-    background-image: url('assets/pattern.png');
-  }
-}
-.container {
-  width: 90%;
-  margin: 0 auto;
-}
 
 // Import Bulma's core
 @import '~bulma/sass/utilities/_all';
