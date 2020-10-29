fb05c0a19b003ca320327f1a4a38179ac6ef9ff4
Kacper Stawinski <stawinskidev@gmail.com>
2020-10-29 11:18:57 +0100

    fix(form-success): change button style

--- src/components/FormSuccess.vue
+++ src/components/FormSuccess.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="success-container success-container_code">
       <div class="success-buttonsRow">
-        <b-button type="is-primary" size="is-medium" @click="download()">Download</b-button>
+        <b-button type="is-primary" size="is-medium" inverted @click="download()">Download</b-button>
         <b-button type="is-primary" size="is-medium" @click="copyToClipboard()">Copy to clipboard</b-button>
         <b-tooltip label="Work in progress" type="is-dark" animated>
           <b-button type="is-primary" size="is-medium" inverted @click="terminalActive = true">Download using terminal</b-button>
