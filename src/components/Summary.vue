1eb126925ecd37621dc2dc7a0b67cc47b8921bbc
Kacper Stawinski <stawinskidev@gmail.com>
2020-10-30 17:16:36 +0100

    refactor(summary): language fix

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -92,7 +92,7 @@ export default {
 
         // If copied, show snackbar
         this.$buefy.snackbar.open({
-          message: 'Content successfully copied to clipboard.',
+          message: 'Content was successfully copied to clipboard.',
           position: 'is-bottom',
           actionText: null,
           duration: 2500,
